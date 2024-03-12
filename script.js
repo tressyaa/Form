626f600509ce65094c5eb4076e5bbaedfa9502fa
tressyaa <152782112+tressyaa@users.noreply.github.com>
2024-03-12 22:51:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function validation() {
+    alert ("Thank you for submitting!")
+}
+
+document.querySelector('.form').reset ();
+
+function validateNumberInput(event) {
+    return event.charCode >= 48 && event.charCode <= 57;
+}
+
+function validateNumberInput(event) {
+    return event.charCode >= 48 && event.charCode <= 57;
+}
